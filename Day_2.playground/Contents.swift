af0c1aea9a70abcbcaf8a11a97f3c62ec497f391
Tanveer Singh Mahendra <mtanveersingh@hotmail.com>
2019-11-28 14:10:10 +0100

    Sets

--- Day_2.playground/Contents.swift
+++ Day_2.playground/Contents.swift
@@ -8,3 +8,9 @@ let vishu = "Vishal Lalwani"
 let waddu = "Mohit Wadhwani"
 
 let darlings = [guptaji, baba, vishu, waddu]
+
+//sets are used to store unique values (duplicates are ignored)
+//sets do not guarantee the order of the items in which they were stored
+//hence items cannot be accessed using index values (like in arrays)
+
+let venn = Set(["red", "green", "blue", "red"]) //ignores second ccurence of red
