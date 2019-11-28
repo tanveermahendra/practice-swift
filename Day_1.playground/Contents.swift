a8dcbbb647908dd9263f7716f355610331145822
Tanveer Singh Mahendra <mtanveersingh@hotmail.com>
2019-11-28 13:04:19 +0100

    Intial creation and push

---
+++ Day_1.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var str = "Hello, playground"
+var temp = "tanx"
+var age = 26
+var population = 8_000_000
+
+var paragraph = """
+This is a long paragraph which contains
+actual line breaks in the print as well
+"""
+
+var longSentence = """
+This is a long sentence \
+written over multiple lines \
+so as to have better code readability
+"""
+
+var pi = 1.61803398875
+
+var bool = true
+var anotherBool = false
+
+var welcomeMessage = "Hello I'm \(temp)"
+
+let cannotChange = "Inevitable"
+
+//cannotChange = "Hahahaha error!"
+
+var autoTypeInference = "This is a string for swift"
+
+// autoTypeInference = true <- won't work.
+
+var selfTypeInference : Int = 5317
+
+//Good for double type which looks like integer
+
+var doubleAsInteger : Double = 999
+
+doubleAsInteger = 22.87 //can be treated as double later
+
+
