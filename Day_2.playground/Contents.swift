8c8c17fa144e89cbe2bea58eefa945f25b798257
Tanveer Singh Mahendra <mtanveersingh@hotmail.com>
2019-11-28 13:47:59 +0100

    Day 2 starts

---
+++ Day_2.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let guptaji = "Ashish Gupta"
+let baba = "Abhilash Nyati"
+let vishu = "Vishal Lalwani"
+let waddu = "Mohit Wadhwani"
+
+let darlings = [guptaji, baba, vishu, waddu]
