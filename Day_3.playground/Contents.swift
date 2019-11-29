af3d35b85b1aa0f3fad56b2bb3f2f3a92bdc7589
Tanveer Singh Mahendra <mtanveersingh@hotmail.com>
2019-11-29 19:36:00 +0100

    Day3 - Operator OVerloading

---
+++ Day_3.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+//operator overloading
+var greet = "Hello "
+var user_name = "Tanveer"
+
+let result = greet + user_name
+
+let arr1 = ["cinnamon", "pepper"]
+let arr2 = ["turmeric", "garam masala"]
+
+let spices = arr1 + arr2
